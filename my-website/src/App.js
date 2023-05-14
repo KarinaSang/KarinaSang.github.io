8348f8b9900457b94380a17225a460c0952a30c3
Karina Sang <ycsang@edu.uwaterloo.ca>
2023-05-14 15:12:30 -0400

    modified app.js

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React, it is bad for your health
         </a>
       </header>
     </div>
